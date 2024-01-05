b8026d0d175dd6536c5af36e1b6d02f00cfb340e
Khun <khunsuk@eueu.com>
2024-01-05 14:44:13 +0700

    Updated

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,81 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+
+    double sum = 0;
+
+    for(int i = 0; i < N ; i++){
+
+        sum += A[i];
+
+    }
+
+    B[0] = sum/N;
+
+    double eueisum = 0;
+
+    for(int i = 0; i < N ; i++){
+
+        double euei = (A[i] - B[0]);
+
+        double wow = pow(euei,2);
+
+        eueisum += wow; 
+
+    }
+
+    double parepueng = eueisum/N;
+    
+    B[1] = sqrt(parepueng);
+
+    double summuply = 1;
+
+    for(int i = 0; i < N ; i++){
+
+        summuply *= A[i];
+
+    }
+
+    B[2] = pow(summuply, 1.0/N);
+
+    double sumsouth = 0;
+
+    for(int i = 0 ; i < N ; i++){
+
+        sumsouth += 1/A[i];
+
+    }
+
+    B[3] = N/sumsouth;
+
+    double Max = A[0];
+
+    for(int i = 1 ; i < N+1 ; i++){
+
+        if(Max < A[i-1]){
+
+            Max = A[i-1];
+
+        }
+    }
+
+    B[4] = Max;
+
+    double Min = A[0];
+    for(int i = 1 ; i < N+1 ; i++){
+
+        if(Min > A[i-1]){
+
+            Min = A[i-1];
+
+
+        }
+
+    }
+
+    B[5] = Min;
+
+
+}
