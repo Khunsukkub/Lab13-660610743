d0bbc1fbd0a70033c9e8d082aca238d57d2b9211
Khun <khunsuk@eueu.com>
2024-01-05 15:34:29 +0700

    Updated

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,38 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool image[N][M],int rad,int x,int y){
+
+    if(rad == 1){
+        image[x][y] = 1;
+    } else{
+        for(int i = 0; i < N ; i++){
+            for(int j = 0; j < M ; j++){
+                if(sqrt(pow(i-x, 2) + pow(j-y, 2)) <= rad-1){
+                    image[i][j] = 1;
+                }
+            }
+        }
+    }
+
+}
+
+void showImage(const bool image[N][M]){
+
+    cout << "------------------------------------------------------------------------" << endl;
+
+    for(int i = 0 ; i < N ; i++){
+        cout << "|";
+        for(int j = 0; j < M ; j++){
+            if(image[i][j]){
+                cout << "*";
+            } else{
+                cout << " ";
+            }
+        }
+        cout << "|" << endl;
+    }
+
+    cout << "------------------------------------------------------------------------" << endl;
+}
